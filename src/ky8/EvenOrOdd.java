a24fb152b7c2325f075782d229069adc9f61f781
Nazarov Mikhail <nazarovmisha@inbox.ru>
2023-01-20 15:28:19 +0300

    8ky

---
+++ src/ky8/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package ky8;
+
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/java/63ca8491d0ffff0058d005ee
+
+//Create a function that takes an integer as an argument and returns "Even" for
+// even numbers or "Odd" for odd numbers.
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+
+        } else {
+            return "Odd";
+        }
+    }
+}
