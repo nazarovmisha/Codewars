51fabea81b068a2d47bc8b612424df908d2624f7
Nazarov Mikhail <nazarovmisha@inbox.ru>
2023-01-20 16:04:25 +0300

    7ky

--- src/ky7/Square.java
+++ src/ky7/Square.java
@@ -1,5 +1,5 @@
-package ky8;
-
+package ky7;
+//https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/java/63ca890293b0b412fabafb32
 //A square of squares
 //        You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
 //
