feb1255dc27048d67dd0439aaa1847a9597c76ea
Nazarov Mikhail <nazarovmisha@inbox.ru>
2023-01-20 17:10:12 +0300

    7ky

---
+++ src/ky7/Vowels.java
@@ -0,0 +1,36 @@
+package ky7;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/java
+//Return the number (count) of vowels in the given string.
+//
+//        We will consider a, e, i, o, u as vowels for this Kata (but not y).
+//
+//        The input string will only consist of lower case letters and/or spaces.
+public class Vowels {
+    public static void main(String[] args) {
+        System.out.println(getCount("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"));
+    }
+
+//    public static int getCount(String str) {
+//        int count = 0;
+//        Pattern pattern = Pattern.compile("[aeiou]");
+//        Matcher matcher = pattern.matcher(str);
+//        while (matcher.find()) {
+//            count++;
+//        }
+//        return count;
+
+    public static int getCount(String str) {
+        int count = 0;
+        char[] c_arr = str.toCharArray();
+        for (int i = 0; i < c_arr.length; i++) {
+            if (c_arr[i] == 'a' || c_arr[i] == 'e' || c_arr[i] == 'i' || c_arr[i] == 'o' || c_arr[i] == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
