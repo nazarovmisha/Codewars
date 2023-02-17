7778fd8e7fb20278e073427510ba0bf6752116a7
Nazarov Mikhail <nazarovmisha@inbox.ru>
2023-02-17 12:33:35 +0300

    6 kyu

--- src/kyu6/ArithmeticProgression.java
+++ src/kyu6/ArithmeticProgression.java
@@ -17,24 +17,39 @@ import java.util.Arrays;
 //        I found it quite fun to solve on paper using math, derive the algo that way. Have fun!
 public class ArithmeticProgression {
     public static void main(String[] args) {
-        System.out.println(findMissing(new int[]{2, 4, 6, 8, 10}));
+        System.out.println(findMissing(new int[]{47, 437, 632, 827, 1022, 1217, 1412, 1607, 1802, 1997, 2192, 2387, 2582, 2777, 2972, 3167, 3362, 3557, 3752}));
     }
 
     public static int findMissing(int[] numbers) {
         int result = 0;
         int count = 0;
-        int trueCount;
+        int finalResult = 0;
+
         Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
         for (int i = 0; i < numbers.length - 1; i++) {
             count = numbers[numbers.length - 1] - numbers[numbers.length - 2];
             result = numbers[1] - numbers[0];
+            if (numbers[i]==numbers[i+1]) {
+                finalResult=numbers[i];
+            }
         }
-        if (result > count) {
-            result = numbers[numbers.length - 1] - numbers[0];
-        } else
-            System.out.println(count);
-        System.out.println(result);
-        return result;
+        if (result < count) {
+            finalResult = numbers[numbers.length - 1] - result;
+        } else if (result > count) {
+            finalResult = numbers[1] - count;
+        } else if (result == count) {
+            for (int i = numbers.length; i >1; i--) {
+                if (result != numbers[i-1] - numbers[i - 2]) {
+                    if(numbers[i-1]>0&&numbers[i-2]>0) {
+                        finalResult = numbers[i - 1] - count;
+                    }else {
+                        finalResult=numbers[i-2]+result;
+                    }
+                }
+            }
+        }
+        return finalResult;
     }
 }
 
