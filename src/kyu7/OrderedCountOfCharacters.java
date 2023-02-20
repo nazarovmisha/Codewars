b13aee4abe5b969bee07f3518e87dde6bc2f31f2
Nazarov Mikhail <nazarovmisha@inbox.ru>
2023-02-20 14:36:55 +0300

    6 kyu

---
+++ src/kyu7/OrderedCountOfCharacters.java
@@ -0,0 +1,29 @@
+package kyu7;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class OrderedCountOfCharacters {
+    public static void main(String[] args) {
+        System.out.println(orderedCount("abracadabra"));
+    }
+
+    public static List<Pair<Character, Integer>> orderedCount(String text) {
+        List<Pair<Character, Integer>> list = new ArrayList<>();
+        char[] arr = text.toCharArray();
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            int k = 0;
+            if (arr[i] == arr[i + 1]) {
+                k++;
+                list.add(k, arr[i]);
+            }
+        }
+        return list;
+    }
+}
+
