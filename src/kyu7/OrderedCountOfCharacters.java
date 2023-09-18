3f91283e5738b308a4310e1aec887dd8f6ac517f
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-09-18 11:06:36 +0300

    8 kyu

--- src/kyu7/OrderedCountOfCharacters.java
+++ src/kyu7/OrderedCountOfCharacters.java
@@ -14,15 +14,25 @@ public class OrderedCountOfCharacters {
 
     public static List<Pair<Character, Integer>> orderedCount(String text) {
         List<Pair<Character, Integer>> list = new ArrayList<>();
+        char left = 0;
+        int right = 1;
+
         char[] arr = text.toCharArray();
+        Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
-        for (int i = 0; i < arr.length; i++) {
-            int k = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] == arr[i + 1]) {
-                k++;
-            //    list.add(k, arr[i]);
+                left = arr[i];
+                right++;
+                int count = (i+1);
+            } else {
+                break;
             }
         }
+
+
+
         return list;
     }
 }
---
+++ src/kyu8/Pillars.java
@@ -0,0 +1,20 @@
+package kyu8;
+//https://www.codewars.com/kata/5bb0c58f484fcd170700063d/train/java
+//There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+//
+//        number of pillars (≥ 1);
+//        distance between pillars (10 - 30 meters);
+//        width of the pillar (10 - 50 centimeters).
+//        Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+
+
+
+public class Pillars {
+    public static void main(String[] args) {
+        System.out.println(pillars(2,20,25));
+    }
+        public static int pillars(int numPill, int dist, int width){
+            return numPill*dist*10* ;
+        }
+    }
+
