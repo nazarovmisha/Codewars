0b1df293ce124ea9fee650bc38b3e9cfe5ea9437
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-09-20 12:59:14 +0300

    8 kyu

---
+++ src/kyu8/DoubleInteger.java
@@ -0,0 +1,12 @@
+package kyu8;
+
+//Code as fast as you can! You need to double the integer and return it.
+
+public class DoubleInteger {
+    public static void main(String[] args) {
+        System.out.println(doubleInteger(-153));
+    }
+    public static int doubleInteger(int i) {
+        return i*2;
+    }
+}
