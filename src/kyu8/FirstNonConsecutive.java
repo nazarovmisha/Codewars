09878dd33de2f1db17388b5fe449b2f969191271
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-09-20 12:52:58 +0300

    8 kyu

--- src/kyu8/FirstNonConsecutive.java
+++ src/kyu8/FirstNonConsecutive.java
@@ -2,17 +2,12 @@ package kyu8;
 
 public class FirstNonConsecutive {
     public static void main(String[] args) {
-        System.out.println(find(new int[]{6334, 6335, 6336, 6337, 6338, 6339, 6340, 6342, 6343}));
-    }
-
+        System.out.println(find(new int[]{6334, 6335, 6336, 6337, 6338, 6339, 6340, 6342, 6343}));    }
 
     static Integer find(final int[] array) {
-
         for (int i = 0; i < array.length - 1; i++)
-         return    (array[i] - array[i + 1] != -1) ? array[i + 1] : 0;
-
-
+            if (array[i] - array[i + 1] != -1)
+                return array[i + 1];
+        return null;
     }
 }
-
-
