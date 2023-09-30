01f6e3ed0d44eefc9462bc62ee60f35fdd98a213
Nazarov Mikhail <nazarovmisha@inbox.ru>
2023-09-30 21:08:31 +0300

    7kyu

---
+++ src/kyu7/ClosingInSum.java
@@ -0,0 +1,55 @@
+package kyu7;
+
+//https://www.codewars.com/kata/65126d52a5de2b11c94096d2/train/java
+
+//Create a function that returns the sum of the digits formed from the first and last digits, all the way to the center of the number.
+//
+//        Worked Example
+//        2520 ➞ 72
+//
+//        # The first and last digits are 2 and 0.
+//        # 2 and 0 form 20.
+//        # The second digit is 5 and the second to last digit is 2.
+//        # 5 and 2 form 52.
+//
+//        # 20 + 52 = 72
+//        Examples
+//        121 ➞ 13
+//        # 11 + 2
+//
+//        1039 ➞ 22
+//        # 19 + 3
+//
+//        22225555 ➞ 100
+//        # 25 + 25 + 25 + 25
+//        Notes
+//        If the number has an odd number of digits, simply add on the single-digit number in the center (see example #1).
+//        Any number which is zero-padded counts as a single digit (see example #2).
+
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ClosingInSum {
+    public static void main(String[] args) {
+        System.out.println(closingInSum(123456l));
+    }
+
+    public static int closingInSum(long n) {
+        String temp = Long.toString(n);
+        int[] numbers = new int[temp.length()];
+        for (int i = 0; i < temp.length(); i++) {
+            numbers[i] = temp.charAt(i) - '0';
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if(i%2==0){
+                System.out.println(numbers[0]+numbers[numbers.length-1]);
+                return  numbers[0]+numbers[numbers.length-1];
+            }
+
+        }
+        return 0;
+    }
+}
+
