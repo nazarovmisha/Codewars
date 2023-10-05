8f0557e9dc13832af55793c7c686af922bae26f1
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-10-05 14:48:47 +0300

    7 kyu

---
+++ src/kyu7/Composing.java
@@ -0,0 +1,14 @@
+package kyu7;
+
+public class Composing {
+    public static void main(String[] args) {
+        System.out.println(compose("byGt\nhTts\nRTFF\nCnnI", "jIRl\nViBu\nrWOb\nNkTB"));
+    }
+    public static String compose(String s1, String s2) {
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s1.length());
+        String result= s1.charAt(0)+s1.substring(s2.(length()-3).
+        return "S";
+    }
+}
