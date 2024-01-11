a03e225797b0339c2c5bac39c26ce5a674ed6a70
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-01-11 14:14:10 +0300

    6 kyu

--- src/kyu6/ReturnRandomly.java
+++ src/kyu6/ReturnRandomly.java
@@ -1,25 +1,30 @@
 package kyu6;
 
+//https://www.codewars.com/kata/593e84f16e836ca9a9000054/train/java
 public class ReturnRandomly {
+    static int result1;
+    static int result2;
+    static int result3;
+
+
     public static void main(String[] args) {
+        System.out.println(oneTwoThree());
+        System.out.println(oneTwoThree());
         System.out.println(oneTwoThree());
 
     }
-    public static int oneTwoThree() {
-        int result1 = 0;
-        int result2 = 0;
-        int result3 = 0;
-        while (true) {
-            if (result1 == result2 && result2 == result3) {
-                result1++;
-                System.out.println(result1);
-                return 1;
-            } else if (result1 > result2 && result2 == result3) {
-                result2++;
-                return 2;
-            }
+    public  static int oneTwoThree() {
+        if (result1 == result2 && result2 == result3) {
+            result1++;
+            return 1;
+        } else if (result1 > result2 && result2 == result3) {
+            result2++;
+            return 2;
+        } else if (result3 < result2 && result2 == result1) {
             result3++;
             return 3;
         }
+        result1++;
+        return 1;
     }
 }
