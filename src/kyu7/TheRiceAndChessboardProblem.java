469e0976e0760de832f82cf489f74c204444e2e3
Nazarov Mikhail <nazarovmisha@inbox.ru>
2024-10-08 19:50:59 +0300

    7

--- src/kyu7/TheRiceAndChessboardProblem.java
+++ src/kyu7/TheRiceAndChessboardProblem.java
@@ -3,21 +3,22 @@ package kyu7;
 //https://www.codewars.com/kata/5b0d67c1cb35dfa10b0022c7/train/java
 public class TheRiceAndChessboardProblem {
     public static void main(String[] args) {
-        System.out.println(squaresNeeded(2));
+        System.out.println(squaresNeeded(10000000000l));
     }
 
     public static int squaresNeeded(long grains) {
-        int cells = 0;
-        int count = 1;
-        int summ = 1;
-        while (summ < grains) {
-            summ += count;
-            System.out.println("summ = " + summ);
-            count *= 2;
+        int cellCapacity = 1;
+        int maxCapacity = 1;
+        int cells = 1;
+        while (maxCapacity<grains){
             cells++;
+            cellCapacity*=2;
+            maxCapacity+=cellCapacity;
+
+        }if (grains==0){
+            return  0;
+        }else {
+            return cells;
         }
-        if (grains == 0) {
-            return 0;
-        } else return cells;
     }
 }
