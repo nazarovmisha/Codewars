d317d6e634038c303d71923889b36e95da436474
Nazarov Mikhail <nazarovmisha@inbox.ru>
2024-10-29 19:44:44 +0300

    HelpYourGranny сделал шапку

---
+++ src/kyu5/HelpYourGranny.java
@@ -0,0 +1,16 @@
+package kyu5;
+
+import java.util.Map;
+
+//https://www.codewars.com/kata/5536a85b6ed4ee5a78000035/train/java
+
+public class HelpYourGranny {
+    public static void main(String[] args) {
+
+    }
+    public static int tour(String[] arrFriends, String[][] ftwns, Map<String, Double> h) {
+        // your code
+        return 1;
+    }
+}
+
