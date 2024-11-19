cf6008179115e64edb2bb8f563b4d3d50207021f
Nazarov Mikhail <nazarovmisha@inbox.ru>
2024-11-19 18:21:02 +0300

    PathFinder3TheAlpinist пока сложно

--- src/kyu4/PathFinder3TheAlpinist.java
+++ src/kyu4/PathFinder3TheAlpinist.java
@@ -6,18 +6,31 @@ import java.util.Arrays;
 
 public class PathFinder3TheAlpinist {
     public static void main(String[] args) {
-        System.out.println(pathFinder("000000\n"+
-                                      "000000\n"+
-                                      "000000\n"+
-                                      "000010\n"+
-                                      "000109\n"+
+        System.out.println(pathFinder("000000\n" +
+                                      "000000\n" +
+                                      "000000\n" +
+                                      "000010\n" +
+                                      "000109\n" +
                                       "001010"));
     }
 
     static int pathFinder(String maze) {
-       String[][] arr = maze.split("");
-        System.out.println(Arrays.toString(arr));
+        String[] arr = maze.split("\n");
+        String[][] matrix = new String[arr.length][];
+        int n = 0;
+        for (String s : arr) {
+            matrix[n++] =Arrays.toString(s.split("");
+        }
+        int startPosition = Integer.parseInt(matrix[0][0]);
+        int finishPosition=Integer.parseInt(matrix[0][0]);
+        while (checkPosition(startPosition)){
+
+        }
         return -1;
+    }
+
+    public static boolean checkPosition (int n){
+
     }
 }
 
