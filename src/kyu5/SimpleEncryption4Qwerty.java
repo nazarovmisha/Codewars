320f9abe384df57385ac5061331acf2acfe43c4f
Nazarov Mikhail <nazarovmisha@inbox.ru>
2024-11-26 19:27:50 +0300

    SimpleEncryption4Qwerty шапка

---
+++ src/kyu5/SimpleEncryption4Qwerty.java
@@ -0,0 +1,19 @@
+package kyu5;
+
+//https://www.codewars.com/kata/57f14afa5f2f226d7d0000f4/train/java
+
+public class SimpleEncryption4Qwerty {
+    public static void main(String[] args) {
+        System.out.println(encrypt(">fdd", 134));
+        System.out.println(decrypt(encrypt(">fdd", 134), 134));
+    }
+
+    public static String encrypt(String text, int key) {
+        return text;
+    }
+
+    public static String decrypt(String encryptedText, int key) {
+        return encryptedText;
+    }
+}
+
