970338320c1486bbfde72b28eca06fe8eed63f19
Nazarov Mikhail <nazarovmisha@inbox.ru>
2024-11-27 21:29:30 +0300

    SimpleEncryption4Qwerty шапка

--- src/kyu5/SimpleEncryption4Qwerty.java
+++ src/kyu5/SimpleEncryption4Qwerty.java
@@ -3,12 +3,22 @@ package kyu5;
 //https://www.codewars.com/kata/57f14afa5f2f226d7d0000f4/train/java
 
 public class SimpleEncryption4Qwerty {
+    static String[] arr1 = {"q", "w", "e", "r", "t", "y", "u", "i", "o", "p"};
+
     public static void main(String[] args) {
         System.out.println(encrypt(">fdd", 134));
         System.out.println(decrypt(encrypt(">fdd", 134), 134));
     }
 
     public static String encrypt(String text, int key) {
+        for (int i = 0; i <arr1.length ; i++) {
+            for (int j = 0; j < text.length(); j++) {
+                if (arr1[i].equals(text.substring(j,j))){
+
+                }
+
+            }
+        }
         return text;
     }
 
